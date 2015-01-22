20d5535dca447b7982d867dd8a18142fa2cb099f
Andrii Ryzhkov <andrii.ryzhkov@gmail.com>
2015-01-22 11:37:36 +0200

    Add compiled files

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+/**
+ * TSATU Homelayout
+ * https://github.com/andriiryzhkov/tsatu-homelayout/
+ *
+ * Copyright (c) 2015 Andrii Ryzhkov
+ * Licensed under the GPLv2+ license.
+ */
+ 
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
